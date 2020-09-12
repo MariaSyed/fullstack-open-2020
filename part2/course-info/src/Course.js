00b7b4a648a70fab19cd6dc0d3c159f349275a83
Maria Syed <maria_s096@hotmail.com>
2020-09-12 11:14:39 +0300

    Complete 2.1-2.5

---
+++ part2/course-info/src/Course.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>
+      <b>Total of {total} exercises</b>
+    </p>
+  );
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+export default Course;
