0b785d66a9eabf989fa816d38711075ab5c9c206
Maria Syed <maria_s096@hotmail.com>
2020-10-09 15:19:57 +0300

    Complete til 3.2

---
+++ part3/phonebook/backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+const persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
+
+app.get('/api/persons', (_, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
+});
+
+const PORT = 3030;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
