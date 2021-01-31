54086a3891730eb94aff6ef1d9a6c34ed716b81f
Maria Syed <maria_s096@hotmail.com>
2021-01-31 16:54:30 +0200

    Complete 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,24 @@
 const listHelper = require('../utils/list_helper');
 
+const generateBlog = () => ({
+  _id: Math.random().toString(36).substr(2, 9),
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url:
+    'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 5,
+  __v: 0,
+});
+
+const listWithOneBlog = [generateBlog()];
+
+const listWithManyBlogs = [
+  { ...generateBlog(), likes: 5 },
+  { ...generateBlog(), likes: 10 },
+  { ...generateBlog(), likes: 3 },
+  { ...generateBlog(), likes: 2 },
+];
+
 test('dummy returns one', () => {
   const blogs = [];
 
@@ -8,37 +27,6 @@ test('dummy returns one', () => {
 });
 
 describe('total likes', () => {
-  const generateBlog = () => ({
-    _id: Math.random().toString(36).substr(2, 9),
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url:
-      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  });
-
-  const listWithOneBlog = [generateBlog()];
-
-  const listWithManyBlogs = [
-    {
-      ...generateBlog(),
-      likes: 5,
-    },
-    {
-      ...generateBlog(),
-      likes: 10,
-    },
-    {
-      ...generateBlog(),
-      likes: 3,
-    },
-    {
-      ...generateBlog(),
-      likes: 2,
-    },
-  ];
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([]);
     expect(result).toBe(0);
@@ -52,5 +40,22 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs);
     expect(result).toBe(20);
+  });
+});
+
+describe('favourite blog', () => {
+  test('of empty list is undefined', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual(undefined);
+  });
+
+  test('when list has only one blog, equals that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toBe(listWithOneBlog[0]);
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs);
+    expect(result).toBe(listWithManyBlogs[1]);
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,7 +3,15 @@ const dummy = () => 1;
 const totalLikes = (blogs) =>
   blogs.reduce((result, blog) => result + blog.likes, 0);
 
+const favoriteBlog = (blogs) =>
+  blogs.reduce(
+    (favorite, blog) =>
+      favorite && favorite.likes > blog.likes ? favorite : blog,
+    undefined
+  );
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
