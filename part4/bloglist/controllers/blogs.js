7c8ce04c36571f88c082f33a6f6bfd41ce704095
Maria Syed <maria_s096@hotmail.com>
2021-02-01 02:48:38 +0200

    Complete 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,7 +8,7 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   let result;
   try {
     const blog = new Blog(request.body);
@@ -18,6 +18,27 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 
   return response.status(201).json(result);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    const result = await Blog.deleteOne({ _id: request.params.id });
+    if (result.deletedCount === 0) throw new Error('Invalid id');
+  } catch (error) {
+    return response.status(400).json({ error: error.toString() });
+  }
+
+  return response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    request.body,
+    { new: true }
+  );
+
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
