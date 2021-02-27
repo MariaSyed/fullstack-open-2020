163f374da90e391400c170077a53e49950d0f979
Maria Syed <maria_s096@hotmail.com>
2021-02-27 16:56:42 +0200

    Complete til part 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-    {' '}
-    {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const expandedEl = (
+    <div>
+      <p>{blog.url}</p>
+      <p>likes: {blog.likes} <button onClick={() => {}}>like</button></p>
+      <p>{blog.user?.username}</p>
+    </div>
+  )
+
+  const toggleExpanded = () => setExpanded(!expanded);
+
+  return (
+    <div style={{ margin: 10, border: '1px solid', padding: 10 }}>
+       <div>
+         {blog.title + ' ' + blog.author + ' '}
+         <button onClick={toggleExpanded}>{expanded ? 'hide' : 'view'}</button>
+       </div>
+       { expanded && expandedEl}
+    </div>
+  )
+}
 
 export default Blog;
